a6d08e0b9e9e4fb4cb1a64b1e1fae7c74fa23c40
Pratik <gadhavepratik621@gmail.com>
2026-07-14 11:08:13 +0530

    Update email address

--- src/content/social.ts
+++ src/content/social.ts
@@ -1,5 +1,5 @@
 export const social = [
-  { url: "mailto:pratik.gadhave@example.com", name: "mail" },
+  { url: "mailto:gadhavepratik621@gmail.com", name: "mail" },
   { url: "https://github.com/pratikgadhave211", name: "github" },
   { url: "https://www.linkedin.com/in/pratik-gadhave-2b0b76387/", name: "linkedin" },
 ] as const satisfies { url: string; name: "mail" | "github" | "instagram" | "linkedin" | "x" }[];
