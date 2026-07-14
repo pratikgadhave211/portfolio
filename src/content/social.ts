0a6bdc1896f1c1884c63db512207ed0da5f078e4
Pratik <gadhavepratik621@gmail.com>
2026-07-14 11:05:54 +0530

    Update LinkedIn URL and remove Twitter

--- src/content/social.ts
+++ src/content/social.ts
@@ -1,6 +1,5 @@
 export const social = [
   { url: "mailto:pratik.gadhave@example.com", name: "mail" },
   { url: "https://github.com/pratik-gadhave", name: "github" },
-  { url: "https://www.linkedin.com/in/pratik-gadhave/", name: "linkedin" },
-  { url: "https://x.com/pratik_gadhave", name: "x" },
+  { url: "https://www.linkedin.com/in/pratik-gadhave-2b0b76387/", name: "linkedin" },
 ] as const satisfies { url: string; name: "mail" | "github" | "instagram" | "linkedin" | "x" }[];
